33615562a12f0ce94d4e9fb87190e111621e83dd
kktkk <ko.ktk@pm.me>
2021-04-01 15:52:36 +0100

    fixed: bitstamp

--- src/exchanges/bitstamp/tickers.go
+++ src/exchanges/bitstamp/tickers.go
@@ -25,37 +25,38 @@ func GetTickers () (tickers []models.Ticker) {
 		var coin string
 		var cur string
 
+		// log.Println(s)
 		for _, c := range curs {
 			if strings.Contains(s, c) {
 				coin = strings.ToUpper(strings.Split(s, c)[0])
+				if coin == "" {
+					coin = strings.ToUpper(strings.Split(s, c)[1])
+				}
 				cur = strings.ToUpper(c)
 			}
 		}
 		
-		if coin != "" && cur != "" {
-			// log.Println(s, data)
-			bid, _ := strconv.ParseFloat(parsed["bid"].(string), 64)
-			ask, _ := strconv.ParseFloat(parsed["ask"].(string), 64)
-			volume, _ := strconv.ParseFloat(parsed["volume"].(string), 64)
-	
-			newTicker := models.Ticker{
-				Coin: coin,
-				Currency: cur,
-				Symbol: coin + cur,
-				BidPrice: bid,
-				BidQty: 0.0,
-				AskPrice: ask,
-				AskQty: 0.0,
-				BaseVolume: volume,
-				QuoteVolume: 0.0,
-				Exchange: "bitstamp",
-				Timestamp: int(time.Now().Unix()),
-			}
-	
-			tickers = append(tickers, newTicker)
-			time.Sleep((1/700)* time.Second)
-			return tickers
+		// log.Println(s, data)
+		bid, _ := strconv.ParseFloat(parsed["bid"].(string), 64)
+		ask, _ := strconv.ParseFloat(parsed["ask"].(string), 64)
+		volume, _ := strconv.ParseFloat(parsed["volume"].(string), 64)
+
+		newTicker := models.Ticker{
+			Coin: coin,
+			Currency: cur,
+			Symbol: coin + cur,
+			BidPrice: bid,
+			BidQty: 0.0,
+			AskPrice: ask,
+			AskQty: 0.0,
+			BaseVolume: volume,
+			QuoteVolume: 0.0,
+			Exchange: "bitstamp",
+			Timestamp: int(time.Now().Unix()),
 		}
+
+		tickers = append(tickers, newTicker)
+		time.Sleep((1/700)* time.Second)
 	}
-	return nil
+	return tickers
 }
