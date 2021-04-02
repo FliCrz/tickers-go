ab59b2455a4853af0b52ed41f6d035fec28ce760
kktkk <ko.ktk@pm.me>
2021-04-03 00:30:01 +0100

    fixed bitstamp, bittrex and kraken get_trade_url2

--- src/exchanges/bitstamp/trade_url.go
+++ src/exchanges/bitstamp/trade_url.go
@@ -3,6 +3,6 @@ package bitstamp
 import "strings"
 
 func GenerateTradeUrl (coin, cur string) string {
-	url := "https://www.bitstamp.net/markets/" + strings.ToLower(coin) + "/" + strings.ToLower(cur)
+	url := "https://www.bitstamp.net/markets/" + strings.ToLower(cur) + "/" +  strings.ToLower(coin)
 	return url
 }
