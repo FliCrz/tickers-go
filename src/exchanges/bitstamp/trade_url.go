5698a113caecb9f3479dd248ece92dfaaa3fa2c7
kktkk <ko.ktk@pm.me>
2021-05-16 22:29:35 +0100

    fix bitstamp trade_url

--- src/exchanges/bitstamp/trade_url.go
+++ src/exchanges/bitstamp/trade_url.go
@@ -3,6 +3,6 @@ package bitstamp
 import "strings"
 
 func GenerateTradeUrl (coin, cur string) string {
-	url := "https://www.bitstamp.net/markets/" + strings.ToLower(cur) + "/" +  strings.ToLower(coin)
+	url := "https://www.bitstamp.net/markets/" + strings.ToLower(coin) + "/" +  strings.ToLower(cur)
 	return url
 }
